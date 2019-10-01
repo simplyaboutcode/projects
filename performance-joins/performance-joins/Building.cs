15340cafea8f66ddc9fb6b30c805a7909ea4b100
marek lapinski <maro.allegro@gmail.com>
2019-10-01 21:44:46 +0200

    Process data implementation

---
+++ performance-joins/performance-joins/Building.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace performance_joins
+{
+    class Building
+    {
+        public string Id { get; set; }
+        public List<string> Workers { get; set; } = new List<string>();
+    }
+}
---
+++ performance-joins/performance-joins/Worker.cs
@@ -0,0 +1,9 @@
+﻿namespace performance_joins
+{
+    class Worker
+    {
+        public string Id { get; set; }
+        public bool IsEmployed { get; set; }
+        public string BuildingId { get; set; }
+    }
+}
